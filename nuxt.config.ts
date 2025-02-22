0b7b9f049f188aadd90d92bba131c850de0639c9
prokop321 <75861541+prokop321@users.noreply.github.com>
2025-02-22 16:26:29 +0100

    cms

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   app: {
-    baseURL: "/CMSpages/",
+    baseURL: "/CMSPages/",
   },
   modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss", "@nuxtjs/mdc"],
   css: ["~/assets/css/main.scss"],
